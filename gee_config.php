2acc9b481e45e64ebc1602a1a9a21dcc01ce5f86
dilshadahmadadmi <dilshadahmadadmi@gmail.com>
2015-10-03 14:26:15 +0530

    http://totalfree.pimp.mobi/postback/gee_config.php
    Best global worldwide traffic

--- gee_config.php
+++ gee_config.php
@@ -1,8 +1,8 @@
 <?php
 $gee_server = "localhost";
 $gee_port   = 3306;
-$gee_db     = "yourdatabase";
-$gee_table  = "gee_postback";
-$gee_user   = "database_user";
-$gee_pass   = "dabatase_password";
+$gee_db     = "dilshadahmadadmi;
+$gee_table  = "gee_dilshadahmadadmi";
+$gee_user   = "dilshadahmadadmi";
+$gee_pass   = "80901822";
 ?>
